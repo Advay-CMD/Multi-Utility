6e88e2d4b9bc9a5951b46c4ec9799024f3b22943
Advay <Advayjan13@gmail.com>
2025-01-13 10:01:47 +0530

    Created UninstallString.bat

---
+++ UninstallString.bat
@@ -0,0 +1,36 @@
+@echo off
+echo You sure you want to uninstall this program?
+@choice /c YN /m "Do you want to continue?"
+IF %ERRORLEVEL% EQU 2 (
+         echo Cleaning Up...
+         TIMEOUT /T 5
+         exit /b
+) ELSE (
+         break
+)
+:: This will uninstall the Multi_Utility Completely
+echo Last Warning! DO YOU WANT TO UNINSTALL THIS PROGRAM
+IF %ERRORLEVEL% EQU 2 (
+         echo Cleaning Up...
+         TIMEOUT /T 5
+         exit /b
+) ELSE (
+         break
+)
+:: Actually, I don't want the user to uninstall this software
+
+rmdir /s /q %systemroot%\System32\Multi_Utility >null
+
+reg delete "HKCU\Software\Microsoft\Windows\CurrentVersion\App Paths\MultiUtility.bat" /f >null
+reg delete "HKCU\Software\Microsoft\Windows\CurrentVersion\Uninstall\MultiUtility" /f >null
+
+echo Done Deleting...
+echo Cleaning up...
+echo Stoping services
+:: This not required but it feels really cool
+timeout /t 5 >null
+echo.
+echo Press any key to continue...
+echo.
+pause
+exit /b
