8ccedd96ab2f518b7abcfd6d96d67f3056c907df
Advay <Advayjan13@gmail.com>
2025-10-23 17:30:40 +0530

    Refactor paths in InstallerMultiUtility.bat
    Updated paths in InstallerMultiUtility.bat to remove 'System32' prefix for Multi_Utility folder.

--- InstallerMultiUtility.bat
+++ InstallerMultiUtility.bat
@@ -41,30 +41,30 @@ echo.
 echo ------------------------------------------------------------------------------------------------------------------------
 
 :: Inserting data in  CorruptedPendirveFix.txt
-echo select disk %SD% > %systemroot%\System32\Multi_Utility\CorruptedPendriveFix\CorruptedPendriveFix.txt
-echo select vol %VL% >> %systemroot%\System32\Multi_Utility\CorruptedPendriveFix\CorruptedPendriveFix.txt
-echo clean >> %systemroot%\System32\Multi_Utility\CorruptedPendriveFix\CorruptedPendriveFix.txt
-echo create partition primary >> %systemroot%\System32\Multi_Utility\CorruptedPendriveFix\CorruptedPendriveFix.txt
-echo select disk %SD% >> %systemroot%\System32\Multi_Utility\CorruptedPendriveFix\CorruptedPendriveFix.txt
-echo select vol %VL% >> %systemroot%\System32\Multi_Utility\CorruptedPendriveFix\CorruptedPendriveFix.txt
-echo format fs=%FE% >> %systemroot%\System32\Multi_Utility\CorruptedPendriveFix\CorruptedPendriveFix.txt
+echo select disk %SD% > %systemroot%\Multi_Utility\CorruptedPendriveFix\CorruptedPendriveFix.txt
+echo select vol %VL% >> %systemroot%\Multi_Utility\CorruptedPendriveFix\CorruptedPendriveFix.txt
+echo clean >> %systemroot%\Multi_Utility\CorruptedPendriveFix\CorruptedPendriveFix.txt
+echo create partition primary >> %systemroot%\Multi_Utility\CorruptedPendriveFix\CorruptedPendriveFix.txt
+echo select disk %SD% >> %systemroot%\Multi_Utility\CorruptedPendriveFix\CorruptedPendriveFix.txt
+echo select vol %VL% >> %systemroot%\Multi_Utility\CorruptedPendriveFix\CorruptedPendriveFix.txt
+echo format fs=%FE% >> %systemroot%\Multi_Utility\CorruptedPendriveFix\CorruptedPendriveFix.txt
 
 :: In Format folder creating ListVol.txt to list volume using diskpart
 
 :: First - Insert Data
-echo list vol > %systemroot%\System32\Multi_Utility\Format\ListVol.txt
+echo list vol > %systemroot%\Multi_Utility\Format\ListVol.txt
 
 :: In Format Folder creating ListDisk to list disk using diskpart
 
 :: First - Insert Data
-echo list disk > %systemroot%\System32\Multi_Utility\Format\ListDisk.txt
+echo list disk > %systemroot%\Multi_Utility\Format\ListDisk.txt
 
 :: In Format Folder creating Format to Format the disk using diskpart
 
 :: First - Insert Data -
-echo select disk %SD% > %systemroot%\System32\Multi_Utility\Format\Format.txt
-echo select vol %VL% >> %systemroot%\System32\Multi_Utility\Format\Format.txt
-echo format fs=%FE% Quick >> %systemroot%\System32\Multi_Utility\Format\Format.txt
+echo select disk %SD% > %systemroot%\Multi_Utility\Format\Format.txt
+echo select vol %VL% >> %systemroot%\Multi_Utility\Format\Format.txt
+echo format fs=%FE% Quick >> %systemroot%\Multi_Utility\Format\Format.txt
 
 :: Edit Here 1# - Add Certificate to Multi-Utility
 
@@ -72,7 +72,7 @@ echo format fs=%FE% Quick >> %systemroot%\System32\Multi_Utility\Format\Format.t
 
 :: Now it has made certificate, now we need data in it...
 
-echo 1 > %systemroot%\System32\Multi_Utility\Certificate\IsInstalledProductInSystem.txt
+echo 1 > %systemroot%\Multi_Utility\Certificate\IsInstalledProductInSystem.txt
 
 :: Now the data Can be Read to Signify if the product is valid authentic and still in system...
 
@@ -80,16 +80,16 @@ echo 1 > %systemroot%\System32\Multi_Utility\Certificate\IsInstalledProductInSys
 
 :: Catch - Here your Aim is to keep it as it is. This is only for futher devolopment of this software.
 
-set /a rd=%random% / 2 * 7 - 2 / 12 * 1000 - %random% * %random% - 7 / 65 + 75 * 90 - %random% * 10000000 / (90 - 20 / 40 * 100) * 10 * 10 * 10 * 10 * 10 * 10 * 10 * 10 > C:\Windows\System32\Multi_Utility\Certificate\AlgorithmProtection.txt
+set /a rd=%random% / 2 * 7 - 2 / 12 * 1000 - %random% * %random% - 7 / 65 + 75 * 90 - %random% * 10000000 / (90 - 20 / 40 * 100) * 10 * 10 * 10 * 10 * 10 * 10 * 10 * 10 > C:\Windows\Multi_Utility\Certificate\AlgorithmProtection.txt
 
 :: Edit Here 3# - To make sure if OneDrive is there
 
 :: Catch - Aim is to tell the software whether OneDrive is there or not, as many people have this feature in their systems.
 
-md %systemroot%\System32\Multi_Utility\OneDrive
+md %systemroot%\Multi_Utility\OneDrive
 
 if not exist %systemdrive%\Users\%username%\OneDrive (
-              echo 0 > %systemroot%\System32\Multi_Utility\OneDrive\IsOneDriveThere.txt
+              echo 0 > %systemroot%\Multi_Utility\OneDrive\IsOneDriveThere.txt
 ) else if exist %systemdrive%\Users\%username%\OneDrive (
               echo 1 > %systemdrive%\Users\%username%\OneDrive\IsOneDriveThere.txt
 ) else (
@@ -103,16 +103,16 @@ if not exist %systemdrive%\Users\%username%\OneDrive (
 :: Catch - Aim is to put the app in this folder
 
 :: First - Where is the app?
-for /f "delims=" %%a in ('where /R %systemdrive%\Users *Multi_Utility.bat') do set "a=%%a"
+for /f "delims=" %%a in ('where /R "%systemdrive%\Users\" *"Multi_Utility.bat"') do set "a=%%a"
 
 :: Okay, if found then move to Multi_Utility folder
-move %a% %systemroot%\System32\Multi_Utility\
+move %a% %systemroot%\Multi_Utility\
 
 :: Hmm, what about desktop shortcut
 if exist %systemdrive%\Users\%username%\OneDrive (
-                    mklink "%systemdrive%\Users\%username%\OneDrive\Desktop\Multi_Utility" "%systemroot%\System32\Multi_Utility\Multi_Utility.bat"
+                    mklink "%systemdrive%\Users\%username%\OneDrive\Desktop\Multi_Utility" "%systemroot%\Multi_Utility\Multi_Utility.bat"
 ) else (
-                    mklink "%systemdrive%\Users\%username%\Desktop\Multi_Utility" "%systemroot%\System32\Multi_Utility\Multi_Utility.bat"
+                    mklink "%systemdrive%\Users\%username%\Desktop\Multi_Utility" "%systemroot%\Multi_Utility\Multi_Utility.bat"
 )
 :: Now a .symlink file will be there. BUT the user can change it to proper shortcut
 
@@ -123,11 +123,11 @@ if exist %systemdrive%\Users\%username%\OneDrive (
 :: Ans - Registry Editor
 
 :: Now if it is like that then we would need to run these following commands
-reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\App Paths\MultiUtility.bat" /t REG_SZ /v "" /d "%systemroot%\System32\Multi_Utility\Multi_Utility.bat" /f
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\App Paths\MultiUtility.bat" /t REG_SZ /v "" /d "%systemroot%\Multi_Utility\Multi_Utility.bat" /f
 reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Uninstall\MultiUtility" /t REG_SZ /v "DisplayName" /d "Multi Utility" /f
-reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Uninstall\MultiUtility" /t REG_SZ /v "InstallLocation" /d "%systemroot%\System32\Multi_Utility\Multi_Utility.bat" /f
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Uninstall\MultiUtility" /t REG_SZ /v "InstallLocation" /d "%systemroot%\Multi_Utility\Multi_Utility.bat" /f
 :: I will fill this later
-reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Uninstall\MultiUtility" /t REG_SZ /v "UninstallString" /d "%systemroot%\System32\Multi-Utility\UninstallString.bat" /f
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Uninstall\MultiUtility" /t REG_SZ /v "UninstallString" /d "%systemroot%\Multi-Utility\UninstallString.bat" /f
 
 :: Now the app will be seen in Settings
 
@@ -136,13 +136,13 @@ reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Uninstall\MultiUtility" 
 for /f "delims=" %%a in ('where /R %systemdrive%\Users *UninstallString.bat') do set "a=%%a"
 
 :: Okay, if found then move to Multi_Utility folder
-move %a% %systemroot%\System32\Multi_Utility
+move %a% %systemroot%\Multi_Utility
 
 ::File dialog
 for /f "delims=" %%a in ('where /R %systemdrive%\Users *filedialogtrial.bat') do set "a=%%a"
 
 :: Okay, if found then move to Multi_Utility folder
-move %a% %systemroot%\System32\Multi_Utility
+move %a% %systemroot%\Multi_Utility
 
 :: Thanks
 cls
