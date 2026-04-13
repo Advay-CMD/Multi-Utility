19a6bd90f4033e96a532ee5cae22eeb7372ecd14
Advay <Advayjan13@gmail.com>
2026-04-13 23:12:57 +0530

    Create File Hider batch script
    AIM: Get this program Multi-Utility into small pieces, for convenience...

---
+++ Programs/FileHider.bat
@@ -0,0 +1,14 @@
+:: File Hider
+title File Hider
+
+echo.
+echo Please Wait ./././././././././././././././././.
+call "C:\Windows\System32\Multi_Utility\filedialogtrial.bat"
+echo.
+attrib +h +s "%resultA%"
+echo.
+echo Hidden. If not probably acess is denied.
+
+echo.
+pause
+echo.
