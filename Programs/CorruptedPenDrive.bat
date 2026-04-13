ea24d0bc74cc03818cec1abac79f3663a20966be
Advay <Advayjan13@gmail.com>
2026-04-13 22:55:12 +0530

    Add CorruptedPenDrive.bat for pen drive fixing utility
    AIM: Get this program Multi-Utility into small pieces, for convenience...

---
+++ Programs/CorruptedPenDrive.bat
@@ -0,0 +1,29 @@
+:: CorruptedPenDrive Fixer
+set FORMAT=%systemroot%\System32\Multi_Utility\Format
+
+title Corrupted pen drive fixer
+
+echo.
+echo Put the corrupted pen drive...
+echo.
+pause
+echo.
+echo Enter what to format to ntfs,fat32,fat(if the name is not out of these 3 it will result in program exit):
+set /p "FE=Enter:"
+Diskpart < C:\Windows\System32\Multi_Utility\Format\ListDisk.txt
+echo.
+echo Select Disk - 
+set /p "SD=Enter Disk No:"
+echo.
+Diskpart < C:\Windows\System32\Multi_Utility\Format\ListVol.txt
+echo.
+echo Select Volume - 
+set /p "VL=Enter Volume No:"
+echo.
+echo. Fixing...
+Diskpart < C:\Windows\System32\Multi_Utility\CorruptedPendriveFix\CorruptedPendriveFix.txt
+echo.
+echo Fixed! If it hasn't been fixed it can be because of the pendrive permanently damaged! Please go to a technical store if not fixed!
+echo.
+pause
+echo.
