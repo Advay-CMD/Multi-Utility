a9ddedd8ce82965212ef923923ed92eda4bfdb75
Advay <Advayjan13@gmail.com>
2026-04-14 15:33:45 +0530

    Refactor SysReport.bat for improved date handling

--- Programs/SysReport.bat
+++ Programs/SysReport.bat
@@ -1,102 +1,105 @@
+@echo off
 :: System Report Script
 title System Report
+setlocal DisableDelayedExpansion
 
-set time=%time:~0,2%%time:~3,2%%time:~6,2%%time:~9,4%
-:: Removing Semicolons from Time
+:: Generate safe date and time values
+for /f %%i in ('wmic os get LocalDateTime ^| find "."') do set "dt=%%i"
+
+if not defined dt (
+    echo Error: Unable to retrieve system time.
+    pause
+    exit /b
+)
+
+set "YYYY=%dt:~0,4%"
+set "MM=%dt:~4,2%"
+set "DD=%dt:~6,2%"
+set "HH=%dt:~8,2%"
+set "Min=%dt:~10,2%"
+set "Sec=%dt:~12,2%"
+
+set "dateStamp=%DD%-%MM%-%YYYY%"
+set "timeStamp=%HH%-%Min%-%Sec%"
 
 :: Folder where directories will be saved
-set reportFile=C:\Windows\System32\Multi_Utility\reportFile
-set reportDir=%reportFile%\SysReport
-set reportPath=%reportDir%_Date_%date%_Time_%time%.txt
+set "reportFile=C:\Windows\System32\Multi_Utility\reportFile"
+set "reportDir=%reportFile%\SysReport"
+set "reportPath=%reportDir%\SysReport_Date_%dateStamp%_Time_%timeStamp%.txt"
 
 :: Create the folder where directories will be saved if it does not exist
-if not exist %reportFile% (
-    mkdir %reportFile%
-)
+if not exist "%reportFile%" mkdir "%reportFile%"
 
 :: Create the reports directory if it doesn't exist
-if not exist %reportDir% (
-    mkdir %reportDir%
-)
-
-:: Reset time
-set time=%time:~0,2%:%time:~3,2%:%time:~6,2%.%time:~9,2%
+if not exist "%reportDir%" mkdir "%reportDir%"
 
 :: Write the new report
 echo Here is the System Report
 echo.
-echo ========================= >> %reportPath%
-echo        SYSTEM REPORT      >> %reportPath%
-echo ========================= >> %reportPath%
-echo. >> %reportPath%
-echo Report Generated on: %date% at %time% >> %reportPath%
-echo. >> %reportPath%
+
+echo ========================= > "%reportPath%"
+echo        SYSTEM REPORT      >> "%reportPath%"
+echo ========================= >> "%reportPath%"
+echo. >> "%reportPath%"
+echo Report Generated on: %dateStamp% at %HH%:%Min%:%Sec% >> "%reportPath%"
+echo. >> "%reportPath%"
 
 :: Gather system information
-echo CPU Information: >> %reportPath%
-echo ------------------ >> %reportPath%
-echo. >> %reportPath%
-wmic cpu Get Name >> %reportPath%
-echo. >> %reportPath%
-echo Load Percentage (%%), Number of Cores: >> %reportPath%
-wmic cpu get loadpercentage, NumberOfCores >> %reportPath%
-echo. >> %reportPath%
+echo CPU Information: >> "%reportPath%"
+echo ------------------ >> "%reportPath%"
+wmic cpu get Name >> "%reportPath%" 2>nul
+echo. >> "%reportPath%"
+echo Load Percentage (%%), Number of Cores: >> "%reportPath%"
+wmic cpu get LoadPercentage,NumberOfCores >> "%reportPath%" 2>nul
+echo. >> "%reportPath%"
 
-echo Battery Information: >> %reportPath%
-echo --------------------- >> %reportPath%
-echo. >> %reportPath%
-wmic path Win32_Battery get Name >> %reportPath%
-echo. >> %reportPath%
-echo Estimated Charge Remaining (%%), Estimated Run Time (minutes): >> %reportPath%
-wmic path Win32_Battery get EstimatedChargeRemaining, EstimatedRunTime >> %reportPath%
-echo. >> %reportPath%
+echo Battery Information: >> "%reportPath%"
+echo --------------------- >> "%reportPath%"
+wmic path Win32_Battery get Name >> "%reportPath%" 2>nul
+echo. >> "%reportPath%"
+echo Estimated Charge Remaining (%%), Estimated Run Time (minutes): >> "%reportPath%"
+wmic path Win32_Battery get EstimatedChargeRemaining,EstimatedRunTime >> "%reportPath%" 2>nul
+echo. >> "%reportPath%"
 
-echo Memory Information: >> %reportPath%
-echo -------------------- >> %reportPath%
-echo. >> %reportPath%
-wmic OS get Name >>%reportPath%
-echo. >> %reportPath%
-echo Free Physical Memory (MB), Total Visible Memory Size (MB): >> %reportPath%
-wmic OS get FreePhysicalMemory, TotalVisibleMemorySize >> %reportPath%
-echo. >> %reportPath%
+echo Memory Information: >> "%reportPath%"
+echo -------------------- >> "%reportPath%"
+wmic os get Name >> "%reportPath%" 2>nul
+echo. >> "%reportPath%"
+echo Free Physical Memory (KB), Total Visible Memory Size (KB): >> "%reportPath%"
+wmic os get FreePhysicalMemory,TotalVisibleMemorySize >> "%reportPath%" 2>nul
+echo. >> "%reportPath%"
 
-echo RAM Information: >> %reportPath%
-echo ------------------ >> %reportPath%
-echo. >> %reportPath%
-wmic MEMORYCHIP get Name >> %reportPath%
-echo. >> %reportPath%
-echo Total RAM Capacity (GB): >> %reportPath%
-wmic MEMORYCHIP get Capacity >> %reportPath%
-echo. >> %reportPath%
+echo RAM Information: >> "%reportPath%"
+echo ------------------ >> "%reportPath%"
+wmic memorychip get Name >> "%reportPath%" 2>nul
+echo. >> "%reportPath%"
+echo Total RAM Capacity (Bytes): >> "%reportPath%"
+wmic memorychip get Capacity >> "%reportPath%" 2>nul
+echo. >> "%reportPath%"
 
-echo GPU Information: >> %reportPath%
-echo ------------------ >> %reportPath%
-echo. >> %reportPath%
-wmic path Win32_VideoController get Name
-echo. >> %reportPath%
-echo Current Refresh Rate (Hz): >> %reportPath%
-wmic path Win32_VideoController get CurrentRefreshRate >> %reportPath%
+echo GPU Information: >> "%reportPath%"
+echo ------------------ >> "%reportPath%"
+wmic path Win32_VideoController get Name >> "%reportPath%" 2>nul
+echo. >> "%reportPath%"
+echo Current Refresh Rate (Hz): >> "%reportPath%"
+wmic path Win32_VideoController get CurrentRefreshRate >> "%reportPath%" 2>nul
+echo. >> "%reportPath%"
 
-echo. >> %reportPath%
-echo ========================= >> %reportPath%
-echo         END OF REPORT     >> %reportPath%
-echo ========================= >> %reportPath%
+echo ========================= >> "%reportPath%"
+echo         END OF REPORT     >> "%reportPath%"
+echo ========================= >> "%reportPath%"
+echo. >> "%reportPath%"
 
-echo. >> %reportPath%
-
-type %reportPath%
+:: Display the generated report
+type "%reportPath%"
 
 :: Ask user if they want to see the full report
 echo.
-CHOICE /C YN /M "Do you want to see the full report (Y/N)?"
-if errorlevel 2 (
-    echo.
-    goto menu
-)
+choice /c YN /m "Do you want to see all saved reports (Y/N)?"
 
-:: Display the full report
-cd %reportFile%
-for %%f in (*.txt) do type "%%f"
+if errorlevel 2 exit /b
+
+:: Display all reports
+for %%f in ("%reportDir%\*.txt") do type "%%f"
 
 pause
-echo.
