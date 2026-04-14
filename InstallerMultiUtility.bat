62f64dbe0632bdca8cd196134c4f6e8c5c904f65
Advay <Advayjan13@gmail.com>
2026-04-14 15:51:41 +0530

    Refactor InstallerMultiUtility.bat for clarity
    Refactor installer script to improve readability and structure. Update commands for creating directories and handling user input.

--- InstallerMultiUtility.bat
+++ InstallerMultiUtility.bat
@@ -1,4 +1,6 @@
 @echo off
+setlocal EnableExtensions EnableDelayedExpansion
+
 echo.
 echo Hello! Welcome to Multi-Utility! Thank you for clicking the Installer
 echo.
@@ -6,31 +8,30 @@ pause
 echo.
 echo Are you sure you want to install Multi-Utility?
 echo Once you approve some settings will be changed that cannot be undone by the uninstaller
-@choice /c YN /m "Do you want to continue?"
-IF %ERRORLEVEL% EQU 2 (
-         TIMEOUT /T 5
-         exit /b
-) ELSE (
-         break
+choice /c YN /m "Do you want to continue?"
+
+IF ERRORLEVEL 2 (
+    TIMEOUT /T 5 >nul
+    exit /b
 )
 
 :: Admin Checkup
 NET SESSION >nul 2>&1
-IF %ERRORLEVEL% EQU 0 (
-    break
-) ELSE (
-         echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\elevate.vbs"
-         echo UAC.ShellExecute "cmd.exe", "/c %~s0", "", "runas", 1 >> "%temp%\elevate.vbs"
-         cscript //nologo "%temp%\elevate.vbs"
-         del "%temp%\elevate.vbs"
-         exit /b
+IF %ERRORLEVEL% NEQ 0 (
+    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\elevate.vbs"
+    echo UAC.ShellExecute "cmd.exe", "/c ""%~f0""", "", "runas", 1 >> "%temp%\elevate.vbs"
+    cscript //nologo "%temp%\elevate.vbs"
+    del "%temp%\elevate.vbs"
+    exit /b
 )
 
 echo.
 echo Setup is starting...
 timeout /t 5 >nul
 echo.
-echo This setup will produce a lot of commands, thus please do not - 1. Close the window 2. Better is not to see those commands and garbage successfulls
+echo This setup will produce a lot of commands, thus please do not:
+echo 1. Close the window
+echo 2. Better is not to see those commands and garbage successfuls
 echo.
 pause
 echo.
@@ -38,141 +39,113 @@ echo The setup will start Now...
 echo.
 echo ------------------------------------------------------------------------------------------------------------------------
 
-:: Inserting data in  CorruptedPendirveFix.txt
-echo select disk %SD% > %systemroot%\System32\Multi_Utility\CorruptedPendriveFix\CorruptedPendriveFix.txt
-echo select vol %VL% >> %systemroot%\System32\Multi_Utility\CorruptedPendriveFix\CorruptedPendriveFix.txt
-echo clean >> %systemroot%\System32\Multi_Utility\CorruptedPendriveFix\CorruptedPendriveFix.txt
-echo create partition primary >> %systemroot%\System32\Multi_Utility\CorruptedPendriveFix\CorruptedPendriveFix.txt
-echo select disk %SD% >> %systemroot%\System32\Multi_Utility\CorruptedPendriveFix\CorruptedPendriveFix.txt
-echo select vol %VL% >> %systemroot%\System32\Multi_Utility\CorruptedPendriveFix\CorruptedPendriveFix.txt
-echo format fs=%FE% >> %systemroot%\System32\Multi_Utility\CorruptedPendriveFix\CorruptedPendriveFix.txt
+:: Define Installation Directory
+set "INSTALL_DIR=%SystemRoot%\System32\Multi_Utility"
+
+:: Create Required Directories
+mkdir "%INSTALL_DIR%" 2>nul
+mkdir "%INSTALL_DIR%\Programs" 2>nul
+mkdir "%INSTALL_DIR%\Format" 2>nul
+mkdir "%INSTALL_DIR%\Certificate" 2>nul
+mkdir "%INSTALL_DIR%\CorruptedPendriveFix" 2>nul
+mkdir "%INSTALL_DIR%\OneDrive" 2>nul
+
+:: Inserting data in CorruptedPendriveFix.txt
+echo select disk %%SD%% > "%INSTALL_DIR%\CorruptedPendriveFix\CorruptedPendriveFix.txt"
+echo select vol %%VL%% >> "%INSTALL_DIR%\CorruptedPendriveFix\CorruptedPendriveFix.txt"
+echo clean >> "%INSTALL_DIR%\CorruptedPendriveFix\CorruptedPendriveFix.txt"
+echo create partition primary >> "%INSTALL_DIR%\CorruptedPendriveFix\CorruptedPendriveFix.txt"
+echo select disk %%SD%% >> "%INSTALL_DIR%\CorruptedPendriveFix\CorruptedPendriveFix.txt"
+echo select vol %%VL%% >> "%INSTALL_DIR%\CorruptedPendriveFix\CorruptedPendriveFix.txt"
+echo format fs=%%FE%% >> "%INSTALL_DIR%\CorruptedPendriveFix\CorruptedPendriveFix.txt"
 
 :: In Format folder creating ListVol.txt to list volume using diskpart
+echo list vol > "%INSTALL_DIR%\Format\ListVol.txt"
 
-:: First - Insert Data
-echo list vol > %systemroot%\System32\Multi_Utility\Format\ListVol.txt
+:: In Format Folder creating ListDisk.txt to list disk using diskpart
+echo list disk > "%INSTALL_DIR%\Format\ListDisk.txt"
 
-:: In Format Folder creating ListDisk to list disk using diskpart
-
-:: First - Insert Data
-echo list disk > %systemroot%\System32\Multi_Utility\Format\ListDisk.txt
-
-:: In Format Folder creating Format to Format the disk using diskpart
-
-:: First - Insert Data -
-echo select disk %SD% > %systemroot%\System32\Multi_Utility\Format\Format.txt
-echo select vol %VL% >> %systemroot%\System32\Multi_Utility\Format\Format.txt
-echo format fs=%FE% Quick >> %systemroot%\System32\Multi_Utility\Format\Format.txt
+:: In Format Folder creating Format.txt to Format the disk using diskpart
+echo select disk %%SD%% > "%INSTALL_DIR%\Format\Format.txt"
+echo select vol %%VL%% >> "%INSTALL_DIR%\Format\Format.txt"
+echo format fs=%%FE%% Quick >> "%INSTALL_DIR%\Format\Format.txt"
 
 :: Edit Here 1# - Add Certificate to Multi-Utility
-
 :: Catch - Aim is to signify that this product is valid and is authentic... and also Installed
-
-:: Now it has made certificate, now we need data in it...
-
-echo 1 > %systemroot%\System32\Multi_Utility\Certificate\IsInstalledProductInSystem.txt
-
-:: Now the data Can be Read to Signify if the product is valid authentic and still in system...
+echo 1 > "%INSTALL_DIR%\Certificate\IsInstalledProductInSystem.txt"
 
 :: Edit Here 2# - Add a product key...
-
-:: Catch - Here your Aim is to keep it as it is. This is only for futher devolopment of this software.
-
-set /a rd=%random% / 2 * 7 - 2 / 12 * 1000 - %random% * %random% - 7 / 65 + 75 * 90 - %random% * 10000000 / (90 - 20 / 40 * 100) * 10 * 10 * 10 * 10 * 10 * 10 * 10 * 10 > C:\Windows\System32\Multi_Utility\Certificate\AlgorithmProtection.txt
+:: Catch - Here your Aim is to keep it as it is. This is only for futher development of this software.
+set /a rd=%random% * %random%
+echo %rd% > "%INSTALL_DIR%\Certificate\AlgorithmProtection.txt"
 
 :: Edit Here 3# - To make sure if OneDrive is there
-
-:: Catch - Aim is to tell the software whether OneDrive is there or not, as many people have this feature in their systems.
-
-md %systemroot%\System32\Multi_Utility\OneDrive
-
-if not exist %systemdrive%\Users\%username%\OneDrive (
-              echo 0 > %systemroot%\System32\Multi_Utility\OneDrive\IsOneDriveThere.txt
-) else if exist %systemdrive%\Users\%username%\OneDrive (
-              echo 1 > %systemdrive%\Users\%username%\OneDrive\IsOneDriveThere.txt
+:: Catch - Aim is to tell the software whether OneDrive is there or not.
+if exist "%UserProfile%\OneDrive" (
+    echo 1 > "%INSTALL_DIR%\OneDrive\IsOneDriveThere.txt"
 ) else (
-              echo Error!
-              echo Exiting in...
-              TIMEOUT /T 7
-              exit
+    echo 0 > "%INSTALL_DIR%\OneDrive\IsOneDriveThere.txt"
 )
 
 :: Edit here 4# - Put the app in the Multi_Utility folder
 :: Catch - Aim is to put the app in this folder
-
-:: First - Where is the app?
-for /f "delims=" %%a in ('where /R %systemdrive%\Users *Multi_Utility.bat') do set "a=%%a"
-
-:: Okay, if found then move to Multi_Utility folder
-move %a% %systemroot%\System32\Multi_Utility\
+for /f "delims=" %%a in ('where /R "%SystemDrive%\Users" Multi_Utility.bat 2^>nul') do (
+    move "%%a" "%INSTALL_DIR%\" >nul 2>&1
+)
 
 :: Hmm, what about desktop shortcut
-if exist %systemdrive%\Users\%username%\OneDrive (
-                    mklink "%systemdrive%\Users\%username%\OneDrive\Desktop\Multi_Utility" "%systemroot%\System32\Multi_Utility\Multi_Utility.bat"
+if exist "%UserProfile%\OneDrive\Desktop" (
+    mklink "%UserProfile%\OneDrive\Desktop\Multi_Utility.bat" "%INSTALL_DIR%\Multi_Utility.bat"
 ) else (
-                    mklink "%systemdrive%\Users\%username%\Desktop\Multi_Utility" "%systemroot%\System32\Multi_Utility\Multi_Utility.bat"
+    mklink "%UserProfile%\Desktop\Multi_Utility.bat" "%INSTALL_DIR%\Multi_Utility.bat"
 )
-:: Now a .symlink file will be there. BUT the user can change it to proper shortcut
 
 :: Edit here 5# - Register the app in settings
 :: Catch - Aim is to put the app in settings to make uninstallation pretty easy
-
-:: First - Where should we register this app?
-:: Ans - Registry Editor
-
-:: Now if it is like that then we would need to run these following commands
-reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\App Paths\MultiUtility.bat" /t REG_SZ /v "" /d "%systemroot%\System32\Multi_Utility\Multi_Utility.bat" /f
-reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Uninstall\MultiUtility" /t REG_SZ /v "DisplayName" /d "Multi Utility" /f
-reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Uninstall\MultiUtility" /t REG_SZ /v "InstallLocation" /d "%systemroot%\System32\Multi_Utility\Multi_Utility.bat" /f
-:: I will fill this later
-reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Uninstall\MultiUtility" /t REG_SZ /v "UninstallString" /d "%systemroot%\System32\Multi-Utility\UninstallString.bat" /f
-
-:: Now the app will be seen in Settings
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\App Paths\MultiUtility.bat" /ve /d "%INSTALL_DIR%\Multi_Utility.bat" /f
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Uninstall\MultiUtility" /v "DisplayName" /d "Multi Utility" /f
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Uninstall\MultiUtility" /v "InstallLocation" /d "%INSTALL_DIR%" /f
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Uninstall\MultiUtility" /v "UninstallString" /d "%INSTALL_DIR%\UninstallString.bat" /f
 
 :: Edit here 6# - Put the Programs in the Programs folder
-mkdir %systemroot%\System32\Multi_Utility\Programs
-for /f "delims=" %%a in ('where /R %systemdrive%\Users *AddAcc.bat') do set "a=%%a"
-move %a% %systemroot%\System32\Multi_Utility\Programs
-for /f "delims=" %%a in ('where /R %systemdrive%\Users *DelAcc.bat') do set "a=%%a"
-move %a% %systemroot%\System32\Multi_Utility\Programs
-for /f "delims=" %%a in ('where /R %systemdrive%\Users *CorruptedPenDrive.bat') do set "a=%%a"
-move %a% %systemroot%\System32\Multi_Utility\Programs
-for /f "delims=" %%a in ('where /R %systemdrive%\Users *FileHider.bat') do set "a=%%a"
-move %a% %systemroot%\System32\Multi_Utility\Programs
-for /f "delims=" %%a in ('where /R %systemdrive%\Users *FileUnhider.bat') do set "a=%%a"
-move %a% %systemroot%\System32\Multi_Utility\Programs
-for /f "delims=" %%a in ('where /R %systemdrive%\Users *FileVirusScan.bat') do set "a=%%a"
-move %a% %systemroot%\System32\Multi_Utility\Programs
-for /f "delims=" %%a in ('where /R %systemdrive%\Users *FormatDisk.bat') do set "a=%%a"
-move %a% %systemroot%\System32\Multi_Utility\Programs
-for /f "delims=" %%a in ('where /R %systemdrive%\Users *NST.bat') do set "a=%%a"
-move %a% %systemroot%\System32\Multi_Utility\Programs
-for /f "delims=" %%a in ('where /R %systemdrive%\Users *SearchFiles.exe') do set "a=%%a"
-move %a% %systemroot%\System32\Multi_Utility\Programs
-for /f "delims=" %%a in ('where /R %systemdrive%\Users *SysInfo.bat') do set "a=%%a"
-move %a% %systemroot%\System32\Multi_Utility\Programs
-for /f "delims=" %%a in ('where /R %systemdrive%\Users *SysReport.bat') do set "a=%%a"
-move %a% %systemroot%\System32\Multi_Utility\Programs
-for /f "delims=" %%a in ('where /R %systemdrive%\Users *ccc.bat') do set "a=%%a"
-move %a% %systemroot%\System32\Multi_Utility\Programs
-for /f "delims=" %%a in ('where /R %systemdrive%\Users *fsvs.bat') do set "a=%%a"
-move %a% %systemroot%\System32\Multi_Utility\Programs
+mkdir "%INSTALL_DIR%\Programs" 2>nul
+
+for %%F in (
+AddAcc.bat
+DelAcc.bat
+CorruptedPenDrive.bat
+FileHider.bat
+FileUnhider.bat
+FileVirusScan.bat
+FormatDisk.bat
+NST.bat
+SearchFiles.exe
+SysInfo.bat
+SysReport.bat
+ccc.bat
+fsvs.bat
+CmdColourChange.bat
+) do (
+    for /f "delims=" %%a in ('where /R "%SystemDrive%\Users" %%F 2^>nul') do (
+        move "%%a" "%INSTALL_DIR%\Programs\" >nul 2>&1
+    )
+)
 
 :: Edit here 7# - Put uninstall string and the file dialog in the Multi_Utility
-:: Uninstall string:
-for /f "delims=" %%a in ('where /R %systemdrive%\Users *UninstallString.bat') do set "a=%%a"
 
-:: Okay, if found then move to Multi_Utility folder
-move %a% %systemroot%\System32\Multi_Utility
+:: Uninstall string
+for /f "delims=" %%a in ('where /R "%SystemDrive%\Users" UninstallString.bat 2^>nul') do (
+    move "%%a" "%INSTALL_DIR%\" >nul 2>&1
+)
 
-::File dialog
-for /f "delims=" %%a in ('where /R %systemdrive%\Users *filedialogtrial.bat') do set "a=%%a"
-
-:: Okay, if found then move to Multi_Utility folder
-move %a% %systemroot%\System32\Multi_Utility
+:: File dialog
+for /f "delims=" %%a in ('where /R "%SystemDrive%\Users" filedialogtrial.bat 2^>nul') do (
+    move "%%a" "%INSTALL_DIR%\" >nul 2>&1
+)
 
 :: Thanks
 cls
 echo.
 echo Done installing! Thank you for choosing this product.
 pause
+exit /b
