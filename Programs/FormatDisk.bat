6efe472a9da9b0f2c89d340ea445c508267a21a9
Advay <Advayjan13@gmail.com>
2026-04-16 00:06:19 +0530

    Update paths to use ProgramFiles variable

--- Programs/FormatDisk.bat
+++ Programs/FormatDisk.bat
@@ -1,23 +1,23 @@
 :: Format Disk
-set FORMAT=%systemroot%\System32\Multi_Utility\Format
+set FORMAT=%ProgramFiles%\Multi_Utility\Format
 
 title Format Disk
 
 echo.
 echo Enter what to format to ntfs,fat32,fat(if the name is not out of these 3 it will result in program exit):
 set /p "FE=Enter:"
-Diskpart < C:\Windows\System32\Multi_Utility\Format\ListDisk.txt
+Diskpart < %ProgramFiles%\Multi_Utility\Format\ListDisk.txt
 echo.
 echo Select Disk - 
 set /p "SD=Enter Disk No:"
 echo.
-Diskpart < C:\Windows\System32\Multi_Utility\Format\ListVol.txt
+Diskpart < %ProgramFiles%\Multi_Utility\Format\ListVol.txt
 echo.
 echo Select Volume - 
 set /p "VL=Enter Volume No:"
 echo.
 echo Formating...
-Diskpart < C:\Windows\System32\Multi_Utility\Format\Format.txt
+Diskpart < %ProgramFiles%\Multi_Utility\Format\Format.txt
 echo.
 echo Formated
 echo.
