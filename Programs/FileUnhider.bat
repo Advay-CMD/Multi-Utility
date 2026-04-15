8a9db88451d866ceb9443fa96580125ed57c89ca
Advay <Advayjan13@gmail.com>
2026-04-16 00:05:23 +0530

    Update file path to use ProgramFiles variable

--- Programs/FileUnhider.bat
+++ Programs/FileUnhider.bat
@@ -3,7 +3,7 @@ title File Unhider
 
 echo.
 echo Please Wait ./././././././././././././././././.
-call "C:\Windows\System32\Multi_Utility\filedialogtrial.bat"
+call "%ProgramFiles%\Multi_Utility\filedialogtrial.bat"
 attrib -h -s "%resultA%"
 echo.
 echo UnHidden. If not probably access is denied.
